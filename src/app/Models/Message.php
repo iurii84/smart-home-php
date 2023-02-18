5ddf82dc03e96af4f2033da83d6dcc9d03545a4e
Iuri <iurii84@gmail.com>
2023-02-18 17:23:08 -0500

    ide-helper files

--- src/app/Models/Message.php
+++ src/app/Models/Message.php
@@ -5,6 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Message
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property int|null $type
+ * @property float $temp
+ * @property float $hum
+ * @property int $compress_ratio
+ * @property string $created
+ * @method static \Illuminate\Database\Eloquent\Builder|Message newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Message newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Message query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereCompressRatio($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereCreated($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereHum($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereTemp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereUuid($value)
+ * @mixin \Eloquent
+ */
 class Message extends Model
 {
     use HasFactory;
