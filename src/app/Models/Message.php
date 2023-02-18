e04fd26b2afdfa80935405c3de6f49fdfe1dd8a0
Iuri <iurii84@gmail.com>
2023-02-18 17:25:45 -0500

    replace qualifiers with imports

--- src/app/Models/Message.php
+++ src/app/Models/Message.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,17 +17,17 @@ use Illuminate\Database\Eloquent\Model;
  * @property float $hum
  * @property int $compress_ratio
  * @property string $created
- * @method static \Illuminate\Database\Eloquent\Builder|Message newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Message newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Message query()
- * @method static \Illuminate\Database\Eloquent\Builder|Message whereCompressRatio($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Message whereCreated($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Message whereHum($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Message whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Message whereTemp($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Message whereType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Message whereUuid($value)
- * @mixin \Eloquent
+ * @method static Builder|Message newModelQuery()
+ * @method static Builder|Message newQuery()
+ * @method static Builder|Message query()
+ * @method static Builder|Message whereCompressRatio($value)
+ * @method static Builder|Message whereCreated($value)
+ * @method static Builder|Message whereHum($value)
+ * @method static Builder|Message whereId($value)
+ * @method static Builder|Message whereTemp($value)
+ * @method static Builder|Message whereType($value)
+ * @method static Builder|Message whereUuid($value)
+ * @mixin Eloquent
  */
 class Message extends Model
 {
